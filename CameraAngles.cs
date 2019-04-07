2b1a7cc3176d65358910417a8673366ec21b8c9f
michael-lutz <43460304+michael-lutz@users.noreply.github.com>
2019-04-07 11:10:45 -0700

    Add files via upload

---
+++ CameraAngles.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleVR;
+
+public class CameraAngles : MonoBehaviour {
+
+
+    /*new Vector3 cameraAngle;
+    private double cameraVertValue;*/
+    public double lookingThresholdDown;
+    public double lookingThresholdUp;
+
+    //private double angle;
+
+    public bool lookingUp;
+
+    //private double accelerating;
+    //public Transform target;
+
+    //private Quaternion quaternion = new Quaternion(0f, 0f, 0f, 1f);
+
+    //private Vector3 rotationVector;
+
+
+    // Use this for initialization
+    void Start () {
+        //rotationVector = transform.
+        transform.eulerAngles = new Vector3(0, 0, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //new Vector3 target = new Quaternion (1, 1, 1, 1);
+        //Debug.Log(transform.eulerAngles);
+        /*cameraAngle = transform.eulerAngles;
+        cameraVertValue = cameraAngle.x;*/
+        //quaternion = transform.rotation;
+
+        //Vector3 angles = quaternion.eulerAngles;
+        //Debug.Log(angles);
+
+        //Debug.Log(angle);
+
+
+        //Debug.Log(Input.acceleration.y);
+        
+        /*if (transform.localEulerAngles.x > lookingThresholdDown && transform.localEulerAngles.x < 90)
+        {
+            Debug.Log("Looking Down (add function later)");
+
+        }
+
+        if (transform.eulerAngles.x < lookingThresholdUp && transform.eulerAngles.x > 270)
+        {
+            //Debug.Log("Looking Up");
+            lookingUp = true;
+        }
+        else
+        {
+            lookingUp = false;
+        }*/
+
+        /*if (transform.eulerAngles.x > 5 && transform.eulerAngles.x < 15)
+        {
+            lookingUp = true;
+        }
+        else
+        {
+            lookingUp = false;
+        }*/
+
+        if (Input.acceleration.y > lookingThresholdUp)
+        {
+            lookingUp = true;
+        }
+        else
+        {
+            lookingUp = false;
+        }
+
+        /*if (transform.localEulerAngles.x < lookingThresholdUp)
+        {
+            //Debug.Log("Looking Up");
+            lookingUp = true;
+        }
+        else
+        {
+            lookingUp = false;
+        }*/
+
+
+    }
+}
---
+++ MovementSystem.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementSystem : MonoBehaviour {
+
+    //public GameObject groundDetector;
+
+    private CollisionFlags m_CollisionFlags;
+    private bool walking = false;
+    public float speed;
+    public float lowestpointonmap;
+
+    private Vector3 spawnPoint;
+
+	// Use this for initialization
+	void Start () {
+        spawnPoint = transform.position;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (walking)
+        {
+            transform.position = transform.position + Camera.main.transform.forward * speed * Time.deltaTime;
+        }
+
+        if (transform.position.y < -10f)
+        {
+            transform.position = spawnPoint;
+            Debug.Log("respawning...");
+        }
+
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(.5f, .5f, 0));
+        RaycastHit hit;
+
+        if (Input.GetMouseButton(0))
+        {
+            walking = true;
+            Debug.Log("running");
+        }
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.name.Contains("plane"))
+            {
+                walking = false;
+                Debug.Log("looking at plane and not walking");
+            }
+
+            Debug.Log("hit something");
+
+        }
+
+
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody body = hit.collider.attachedRigidbody;
+        //dont move the rigidbody if the character is on top of it
+        if (m_CollisionFlags == CollisionFlags.Below)
+        {
+            return;
+        }
+
+        if (body == null || body.isKinematic)
+        {
+            return;
+        }
+        //body.AddForceAtPosition(m_CharacterController.velocity * 0.1f, hit.point, ForceMode.Impulse);
+    }
+}
